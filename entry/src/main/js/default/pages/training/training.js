c92425abef3a9523db756be0b3d431b8975d182a
kopp4 <kopp4@users.noreply.github.com>
2023-05-21 15:41:15 +0800

    TMP

--- entry/src/main/js/default/pages/training/training.js
+++ entry/src/main/js/default/pages/training/training.js
@@ -4,18 +4,25 @@ var picker1value = null;
 var picker2value = null;
 
 var picker1seconds = null;
+var picker2seconds = null;
 
 var timer1 = null;
+var timer2 = null;
+
+var counter = 0;
 
 export default {
     data: {
-        // title: 'World'
         seconds: 0,
-        isShow: true
+        isShow: true,
+        breath: "breath in",
     },
     clickAction() {
         clearInterval(timer1);
         timer1 = null;
+
+        clearInterval(timer2);
+        timer2 = null;
 
         router.replace({
             uri: 'pages/index/index'
@@ -29,15 +36,21 @@ export default {
         picker1value = this.data1;
         picker1value = this.data2;
 
-        if (picker1value == '1') {
-            picker1seconds = 60;
-        } else if (picker1value == '2') {
-            picker1seconds = 120;
-        } else if (picker1value == '3') {
-            picker1seconds = 180;
-        }
         picker1seconds = picker1value * 60;
         this.seconds = picker1seconds;
+
+        if (picker2value == 'S') {
+
+        }
+
+        // if (picker1value == '1') {
+        //     picker1seconds = 60;
+        // } else if (picker1value == '2') {
+        //     picker1seconds = 120;
+        // } else if (picker1value == '3') {
+        //     picker1seconds = 180;
+        // }
+
 
     },
     run1() {
@@ -60,5 +73,20 @@ export default {
     onDestroy() {
         console.log("training.js/onDestroy() is evoked");
     },
+
+    run2() {
+        counter++;
+        if (counter = picker1seconds / picker2seconds) {
+            clearInterval(timer2);
+            timer2 = null;
+            this.breath = 'Done';
+        } else {
+            if (this.breath == 'breath in') {
+                this.breath = 'breath out';
+            } else if (this.breath == 'breath out') {
+                this.breath = 'breath in'
+            }
+        }
+    }
 
 }
