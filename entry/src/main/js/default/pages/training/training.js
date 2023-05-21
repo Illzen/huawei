0fb5d7e6517103e7b5b4250dd2b3dc0e399a37a2
kopp4 <kopp4@users.noreply.github.com>
2023-05-21 14:33:48 +0800

    Lab8 attempt

--- entry/src/main/js/default/pages/training/training.js
+++ entry/src/main/js/default/pages/training/training.js
@@ -1,8 +1,14 @@
 import router from '@system.router';
 
+var picker1value = null;
+var picker2value = null;
+
+var picker1seconds = null;
+
 export default {
     data: {
         // title: 'World'
+        seconds: 0
     },
     clickAction() {
         router.replace({
@@ -13,6 +19,20 @@ export default {
         console.log("training.js/onInit() is evoked");
         console.log("Accepted left selector: " + this.data1);
         console.log("Accepted right selector: " + this.data2);
+
+        picker1value = this.data1;
+        picker1value = this.data2;
+
+        if (picker1value == '1') {
+            picker1seconds = 60;
+        } else if (picker1value == '2') {
+            picker1seconds = 120;
+        } else if (picker1value == '3') {
+            picker1seconds = 180;
+        }
+        picker1seconds = picker1value * 60;
+        this.seconds = picker1seconds;
+
     },
     onReady() {
         console.log("training.js/onReady() is evoked");
