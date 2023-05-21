becf064c253e7c0fc01ff18c5c75decf6a6cda7c
kopp4 <kopp4@users.noreply.github.com>
2023-05-21 17:14:23 +0800

    Lab 15 attempt

--- entry/src/main/js/default/pages/countdown/countdown.js
+++ entry/src/main/js/default/pages/countdown/countdown.js
@@ -1,5 +1,29 @@
+var counter = 3;
+var timer = null;
+
 export default {
     data: {
+        imgsrc: "",
+        seconds: "",
+    },
+    run() {
+        counter = counter - 1;
+        if (counter != 0) {
+            this.imgsrc = "/common/" + "n" + counter.toString() + ".png";
+            this.seconds = counter.toString();
+        } else {
+            this.imgsrc = "";
+            this.seconds = "";
 
-    }
+            clearInterval(timer);
+            timer = null;
+        }
+    },
+    onInit() {
+        this.imgsrc = "/common/" + "n" + counter.toString() + ".png";
+        this.seconds = counter.toString();
+    },
+    onShow() {
+        timer = setInterval(this.run, 1000);
+    },
 }
