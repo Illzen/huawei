9ff487bc639387827aac464ac6d1208cfff14bb7
kopp4 <kopp4@users.noreply.github.com>
2023-05-29 15:44:23 +0800

    Lab19 initial

--- entry/src/main/js/default/pages/report1/report1.js
+++ entry/src/main/js/default/pages/report1/report1.js
@@ -1,11 +1,17 @@
 export default {
     data: {
+        states: [
+            'Desperate 80-99',
+            'Tense 60-79',
+            'Normal 30-59',
+            'Relax 1-29',
+        ]
 
     },
-    toIndexPage(e){
-        if(e.direction == 'left'){
+    toIndexPage(e) {
+        if (e.direction == 'left') {
             router.replace({
-                uri:'pages/index/index'
+                uri: 'pages/index/index'
             });
         }
     }
